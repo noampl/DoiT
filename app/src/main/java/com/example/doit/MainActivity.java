594a454f433900a23ec0ffa4f3d642c4a3a2ced1
Noam Peleg <noampl74@gmail.com>
2021-12-02 21:49:34 +0200

    mid work,
    Create app navGraph and fragment

--- app/src/main/java/com/example/doit/MainActivity.java
+++ app/src/main/java/com/example/doit/MainActivity.java
@@ -1,21 +1,23 @@
 package com.example.doit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.doit.databinding.ActivityMainBinding;
 import com.example.doit.Model.Repository;
 
-public class MainActivity extends AppCompatActivity {
-    private Repository db;
 
-    public MainActivity() {
-        super(R.layout.activity_main);
-    }
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        _binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
