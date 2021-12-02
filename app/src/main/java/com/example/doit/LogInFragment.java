0b4c7a8f069167500d3dc6d9f7e63278e93eea61
Frishman <frishman.itay@gmail.com>
2021-12-02 19:01:43 +0200

    fixed gradle

--- app/src/main/java/com/example/doit/LogInFragment.java
+++ app/src/main/java/com/example/doit/LogInFragment.java
@@ -15,14 +15,7 @@ import android.view.ViewGroup;
  */
 public class LogInFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private View _binding;
 
     public LogInFragment() {
         // Required empty public constructor
@@ -39,10 +32,6 @@ public class LogInFragment extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static LogInFragment newInstance(String param1, String param2) {
         LogInFragment fragment = new LogInFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -50,8 +39,6 @@ public class LogInFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
