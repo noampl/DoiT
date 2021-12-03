ed5c95bcbad5c9028bed2c8e8e747bd661668dea
Frishman <frishman.itay@gmail.com>
2021-12-03 12:12:39 +0200

    added firebase

--- app/src/main/java/com/example/doit/LogInFragment.java
+++ app/src/main/java/com/example/doit/LogInFragment.java
@@ -6,6 +6,8 @@ import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
@@ -48,12 +50,10 @@ public class LogInFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_log_in, container, false);
         LoginViewModel viewModel = new ViewModelProvider(this).get(LoginViewModel.class);
         _binding.setLoginViewModel(viewModel);
         _binding.setLifecycleOwner(this);
-        
         return _binding.getRoot();
     }
 }
--- app/src/main/java/com/example/doit/LoginViewModel.java
+++ app/src/main/java/com/example/doit/LoginViewModel.java
@@ -1,15 +1,39 @@
 package com.example.doit;
 
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class LoginViewModel extends ViewModel {
+    private static final String TAG = "LoginViewModel";
+    private String _userName;
+    private String _password;
 
+    public LoginViewModel() {
+        _userName = "User Name";
+        _password = "";
+    }
 
+    public String getUserName(){
+        return _userName;
+    }
 
+    public String getPassword() {
+        return _password;
+    }
 
+    public void onUserNameChange(CharSequence s, int start, int before, int count) {
+        _userName = s.toString();
+    }
+
+    public void onPasswordChange(CharSequence s, int start, int before, int count) {
+        _password = s.toString();
+    }
 
     public boolean Login(){
-
+        Log.d(TAG, "Login: " + _userName);
+        Log.d(TAG, "Password: " + _password);
         return true;
     }
 }
