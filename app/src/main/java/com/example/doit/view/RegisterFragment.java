c87ae9ec919d3b7c7ca6a54a9332f4d3a9b7878c
Frishman <frishman.itay@gmail.com>
2021-12-31 16:56:51 +0200

    added the ability to add profile picture to app

--- app/src/main/java/com/example/doit/view/RegisterFragment.java
+++ app/src/main/java/com/example/doit/view/RegisterFragment.java
@@ -1,7 +1,19 @@
 package com.example.doit.view;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -26,6 +38,19 @@ import com.example.doit.databinding.FragmentRegisterBinding;
 public class RegisterFragment extends Fragment implements IResponseHelper {
     private FragmentRegisterBinding _binding;
     private RegisterViewModel viewModel;
+    ActivityResultLauncher<Intent> pickPhotoResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        Uri uri = data != null ? data.getData() : null;
+                        _binding.profileImageButton.setImageURI(uri);
+                    }
+                }
+            }
+    );
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -48,6 +73,16 @@ public class RegisterFragment extends Fragment implements IResponseHelper {
         viewModel.setResponseHelper(this);
         _binding.setRegisterViewModel(viewModel);
         _binding.setLifecycleOwner(this);
+        _binding.profileImageButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("IntentReset")
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickPhoto.setType("image/*");
+                pickPhotoResultLauncher.launch(pickPhoto);
+            }
+        });
+
         return _binding.getRoot();
     }
 
