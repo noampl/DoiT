8aeaa4c2888cc00a1ea38e1035716455cc95a0f1
Frishman <frishman.itay@gmail.com>
2022-01-09 16:59:22 +0200

    Query from firebase all tasks of auth user when app is invoke

--- app/src/main/java/com/example/doit/model/Repository.java
+++ app/src/main/java/com/example/doit/model/Repository.java
@@ -137,11 +137,6 @@ public class Repository {
         return _newGroupAdmins;
     }
 
-    public MutableLiveData<List<User>> get_users() {
-        if (_users == null) {_users = new MutableLiveData<>(); }
-        return _users;
-    }
-
     // endregion
 
     // region Public Methods
@@ -160,7 +155,8 @@ public class Repository {
             @Override
             public void run() {
                 saveOrUpdateUser(userFirebaseWorker.getAuthenticatedUserDetails());
-                userFirebaseWorker.getAllAuthUserGroups();
+                //userFirebaseWorker.getAllAuthUserGroups();
+                userFirebaseWorker.getAllAuthUserGroupAndTasks();
 
             }
         }).start();
--- app/src/main/java/com/example/doit/model/firebaseUtils.java
+++ app/src/main/java/com/example/doit/model/firebaseUtils.java
@@ -6,6 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.doit.model.entities.Group;
+import com.example.doit.model.entities.Task;
 import com.example.doit.model.entities.User;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
@@ -27,6 +28,23 @@ public class firebaseUtils {
        newGroup.set_image((String) groupDocument.get("image"));
        newGroup.set_tasksId((List<String>) groupDocument.get("taskId"));
        return newGroup;
+    }
+
+    public static Task convertFirebaseDocumentToTask(DocumentSnapshot taskDocument){
+        Task newTask = new Task();
+        newTask.set_groupId(taskDocument.getId());
+        newTask.set_name((String) taskDocument.get("name"));
+        newTask.set_description((String) taskDocument.get("description"));
+        newTask.set_createdById((String) taskDocument.get("createdByUserId"));
+        newTask.set_assigneeId((String) taskDocument.get("assigneeId"));
+        newTask.set_image((String) taskDocument.get("image"));
+        newTask.set_createdDate(taskDocument.get("createdDate") == null ? 0 : (Long) taskDocument.get("createdDate"));
+        newTask.set_finishDate(taskDocument.get("finishDate") == null ? 0 : (Long) taskDocument.get("createdDate"));
+        newTask.set_targetDate(taskDocument.get("targetDate") == null ? 0 : (Long) taskDocument.get("createdDate"));
+        newTask.set_value(taskDocument.get("value") == null ? 0 : (Integer) taskDocument.get("value"));
+        newTask.set_image((String) taskDocument.get("image"));
+
+        return newTask;
     }
 
     public static EventListener<DocumentSnapshot> getGroupListener(MutableLiveData<User> user){
